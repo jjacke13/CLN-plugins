82e0bcd3e619156e1923747d120e58af08eac2ff
jjacke13 <jjacke@proton.me>
2025-05-13 22:49:07 +0300

    added rebalance plugin

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           payany = import ./payany.nix { inherit pkgs; };
           pyln-client = import ./pyln-client.nix { inherit pkgs; }; #not a plugin, but needed as a python dependency for python plugins
           summary = import ./summary.nix {inherit pkgs; inherit inputs; inherit system; };
+          rebalance = import ./rebalance.nix {inherit pkgs; inherit inputs; inherit system; };
         };
         
         
--- rebalance.nix
+++ rebalance.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, system }:
 
 pkgs.python3Packages.buildPythonApplication {
-  pname = "cln-summary-plugin";
+  pname = "cln-rebalance-plugin";
   version = "1.0";
   format = "other";
 
@@ -12,12 +12,10 @@ pkgs.python3Packages.buildPythonApplication {
     sha256 = "sha256-n+lnpYXc1b4Sy0CfoUDlV7G9vWfgLh1JbtGhnYPzH6w=";
   };
 
-  sourceRoot = "source/summary";
+  sourceRoot = "source/rebalance";
 
   propagatedBuildInputs = [
     inputs.self.packages.${system}.pyln-client
-    pkgs.python3Packages.requests
-    pkgs.python3Packages.packaging
   ];
 
   dontBuild = true;
@@ -25,7 +23,7 @@ pkgs.python3Packages.buildPythonApplication {
   installPhase = ''
     mkdir -p $out/bin
     cp * $out/bin/
-    chmod +x $out/bin/summary.py
+    chmod +x $out/bin/rebalance.py
   '';
 
   doCheck = false;
