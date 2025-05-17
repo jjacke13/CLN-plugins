1f2c09e5031a0acbf54d9833c573f4612765596f
jjacke13 <jjacke@proton.me>
2025-05-17 16:47:24 +0300

    updated trustedcoin to v0.8.4

--- trustedcoin.nix
+++ trustedcoin.nix
@@ -2,16 +2,16 @@
 
 pkgs.buildGoModule {
   pname = "trustedcoin";
-  version = "0.8.3";
+  version = "0.8.4";
 
   src = pkgs.fetchFromGitHub {
     owner = "nbd-wtf";
     repo = "trustedcoin";
-    rev = "v0.8.3";
-    sha256 = "sha256-GGF1PmErDiKHxrq95NQq50ZsdYPRnt+mcg00S9uG7uQ=";
+    rev = "v0.8.4";
+    sha256 = "sha256-mROWJEMRuIAcDXtEyin2+MLFplPInOsv5Q7vACNzi80=";
   };
 
-  vendorHash = "sha256-VbfpVgXnAwhUi06Kd8p/CFkx5t+GBg9GhwLeIDX7dc8="; 
+  vendorHash = "sha256-fW+EoNPC0mH8C06Q6GXNwFdzE7oQT+qd+B7hGGml+hc="; 
   doCheck = false;
   
 }
