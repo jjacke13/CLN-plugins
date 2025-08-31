c281533ec567b6a1c77f470c479c4a9114093976
Vaios K <jjacke@proton.me>
2025-08-31 20:17:41 +0300

    trustedcoin: update to v0.8.6

--- trustedcoin.nix
+++ trustedcoin.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGoModule {
   pname = "trustedcoin";
-  version = "0.8.4";
+  version = "0.8.6";
 
   src = pkgs.fetchFromGitHub {
     owner = "nbd-wtf";
     repo = "trustedcoin";
-    rev = "v0.8.4";
-    sha256 = "sha256-mROWJEMRuIAcDXtEyin2+MLFplPInOsv5Q7vACNzi80=";
+    rev = "v0.8.6";
+    sha256 = "sha256-b+Icq/9qMF+Zvh7RuG9RxU8/U07Tl8ymZvNKWsZzatw=";
   };
 
   vendorHash = "sha256-fW+EoNPC0mH8C06Q6GXNwFdzE7oQT+qd+B7hGGml+hc=";
