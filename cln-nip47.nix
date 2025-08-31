329d13d9053e24656f6cffe829cc9e4f2f7b5510
Vaios K <jjacke@proton.me>
2025-08-31 20:25:49 +0300

    cln-nip47: update to v0.1.5

--- cln-nip47.nix
+++ cln-nip47.nix
@@ -4,13 +4,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "cln-nip47";
-  version = "0.1.4";
+  version = "0.1.5";
 
   src = pkgs.fetchFromGitHub {
     owner = "daywalker90";
     repo = "cln-nip47";
-    rev = "v0.1.4";
-    hash = "sha256-KzJNYJY5yWhqoZL5KVRV6juFhrBdK9UmfBS5KfnUUpg=";
+    rev = "v0.1.5";
+    hash = "sha256-kULaO8Ya4/cTn66FSlSFFvxc9N2O775MzPhbpYBo/oI=";
   };
 
   cargoLock = {
