3a77c9b7205430356ae28040727a1ea8a8eea0fe
Vaios K <jjacke@proton.me>
2025-08-31 20:34:19 +0300

    clnrest: update to v0.2.0

--- clnrest.nix
+++ clnrest.nix
@@ -4,13 +4,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "clnrest";
-  version = "0.1.1";
+  version = "0.2.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "ElementsProject";
     repo = "lightning";
-    rev = "release-v25.02.1";
-    hash = "sha256-XWOtWg4ckXepAS4L2GDl+lMO3FsuyCwVNxgfetdg+Lc=";
+    rev = "v25.05";
+    hash = "sha256-XkEeEOpE+tzQ25vzgH6FfE1FNHgwAVdNd9zhMjgxY18=";
   };
 
   cargoBuildFlags = [ "-p" "clnrest " "--manifest-path" "plugins/rest-plugin/Cargo.toml" ];
