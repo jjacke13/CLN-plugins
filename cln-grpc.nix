6c37a06ef5e93e7bddb3507b427c6d090f46e391
Vaios K <jjacke@proton.me>
2026-03-04 20:54:40 +0200

    update to v0.5.0

--- cln-grpc.nix
+++ cln-grpc.nix
@@ -4,13 +4,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "cln-grpc-plugin";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "ElementsProject";
     repo = "lightning";
-    rev = "v25.05";
-    hash = "sha256-XkEeEOpE+tzQ25vzgH6FfE1FNHgwAVdNd9zhMjgxY18=";
+    rev = "v25.12.1";
+    hash = "sha256-RgfEEwSqp7qM1CwFFlEUkxjEEAxa3A8kbEuVt4mYJVM=";
   };
 
   cargoBuildFlags = [ "-p" "cln-grpc-plugin " "--manifest-path" "plugins/grpc-plugin/Cargo.toml" ];
